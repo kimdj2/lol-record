5742c05259fd4eaee8442b3e563fb50d1542fe52
kimdj2 <1222kdj@gmail.com>
2020-05-19 23:24:30 +0900

    first commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'lol-record-project'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
